c482849834e060e38ebd2ab88eeb36ba97eac323
Sameer Ismail <sam@samism.io>
2019-06-23 14:31:07 -0500

    Add access logging, rate limiting, endpoints.

--- app.js
+++ app.js
@@ -1,27 +1,64 @@
 require('dotenv').config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const rateLimit = require('express-rate-limit');
+const rfs = require('rotating-file-stream');
 
-var indexRouter = require('./routes/index');
+const notificationEndpoints = require('./api');
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+const { NODE_ENV: env } = process.env;
+
+app.use(
+  logger('common', {
+    stream: rfs('access.log', {
+      interval: '1d',
+      path: path.join(__dirname, 'log')
+    })
+  })
+);
+
+if (env === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+if (env !== 'test') {
+  app.use(logger(env));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   express.static(
-    path.join(
-      __dirname,
-      process.env.NODE_ENV === 'production' ? 'build' : 'client/public'
-    )
+    path.join(__dirname, env === 'production' ? 'build' : 'client/public')
   )
 );
 
-app.use('/api', indexRouter);
+app.use(
+  '/api/v1/',
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  }),
+  notificationEndpoints
+);
 
 module.exports = app;
