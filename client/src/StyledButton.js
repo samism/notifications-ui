d50c3afde3f15a0db92815a93bcc63374d4585e1
Sameer Ismail <sam@samism.io>
2019-06-23 23:57:37 -0500

    Increase padding.

---
+++ client/src/StyledButton.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  font-size: 25px;
+  background-color: blue;
+  border-color: blue;
+  border-radius: 3px;
+  color: white;
+  padding: 5px 20px;
+  margin-bottom: 1em;
+`;
+
+export default StyledButton;
