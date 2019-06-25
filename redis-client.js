33ffbe81157bfc10bb2110365d0bc26513590340
Sameer Ismail <sam@samism.io>
2019-06-24 21:28:48 -0500

    Configure + async-ify redis callbacks.

---
+++ redis-client.js
@@ -0,0 +1,14 @@
+const redis = require('redis');
+const { promisify } = require('util');
+const client = redis.createClient(process.env.REDIS_URL);
+
+client.on('error', err => {
+  console.error('Error ' + err);
+});
+
+module.exports = {
+  ...client,
+  getAsync: promisify(client.get).bind(client),
+  setAsync: promisify(client.set).bind(client),
+  keysAsync: promisify(client.keys).bind(client)
+};
