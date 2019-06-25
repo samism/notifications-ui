b9c0c9fb67c392859dd69b98357e3b9861c4975c
Sameer Ismail <sam@samism.io>
2019-06-24 23:10:37 -0500

    Add redis quit command.

--- redis-client.js
+++ redis-client.js
@@ -8,6 +8,7 @@ client.on('error', err => {
 
 module.exports = {
   ...client,
+  quit: promisify(client.quit).bind(client),
   getAsync: promisify(client.get).bind(client),
   setAsync: promisify(client.set).bind(client),
   keysAsync: promisify(client.keys).bind(client),
